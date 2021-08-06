d4dbbcd29ae4f3b15b6f3647f3e34f51d7a8af68
wolfgang2612 <frozenthrone.ashutosh.mishra@gmail.com>
2021-08-06 19:46:55 +0530

    Added github link to header

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -1,7 +1,10 @@
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
 import { makeStyles } from "@material-ui/core/styles";
+
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,6 +28,15 @@ function SiteHeader() {
           <Typography variant="h4" gutterBottom align="center">
             A minimal client to send HTML requests!
           </Typography>
+        </Grid>
+        <Grid container item xs={12} justifyContent="flex-end">
+          <IconButton
+            aria-label="github"
+            href="https://github.com/wolfgang2612/resteasy"
+            target="_blank"
+          >
+            <GitHubIcon />
+          </IconButton>
         </Grid>
       </Paper>
     </Grid>
