37086e809748c47d416ee3dea379f988e775a5ce
wolfgang2612 <frozenthrone.ashutosh.mishra@gmail.com>
2021-08-06 19:36:34 +0530

    Added SiteHeader skeleton

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -1,5 +1,34 @@
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.grey["A100"],
+    width: "100%",
+    margin: "10px",
+  },
+}));
+
 function SiteHeader() {
-  return <div style={{ marginBottom: "10px" }}>Header</div>;
+  const classes = useStyles();
+  return (
+    <Grid container item xs={12}>
+      <Paper elevation={3} className={classes.root}>
+        <Grid container item xs={12} justifyContent="center">
+          <Typography variant="h2" gutterBottom align="center">
+            REST Easy
+          </Typography>
+        </Grid>
+        <Grid container item xs={12} justifyContent="center">
+          <Typography variant="h4" gutterBottom align="center">
+            A minimal client to send HTML requests!
+          </Typography>
+        </Grid>
+      </Paper>
+    </Grid>
+  );
 }
 
 export default SiteHeader;
