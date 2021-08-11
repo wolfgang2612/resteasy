d943ad7a048785232b0c7beb5999a91480bd953e
wolfgang2612 <frozenthrone.ashutosh.mishra@gmail.com>
2021-08-11 19:38:15 +0530

    Removed some useless text from Response

--- src/components/Response.js
+++ src/components/Response.js
@@ -129,13 +129,7 @@ function Response(props) {
         break;
       default:
         return_body = [];
-        return_body.push(
-          "Data is not JSON or text, click the button to download the response data. ",
-        );
-        return_body.push(<br />);
-        return_body.push(
-          "(If you are expecting a file as a response, be sure to put the Content-disposition header = attachment for automatic triggering of file download.)",
-        );
+        return_body.push("Data is not JSON or text, raw data: ");
         return_body.push(<br />);
         return_body.push(<hr />);
         return_body.push(res.data);
